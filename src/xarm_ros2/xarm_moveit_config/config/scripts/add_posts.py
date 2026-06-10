3dde989f8f4f062cbb43f45553c431d97dd1e865
Caden Nurse <cadencgn@gmail.com>
2026-06-10 15:10:44 +0200

    made a cage for the workspace

--- src/xarm_ros2/xarm_moveit_config/config/scripts/add_posts.py
+++ src/xarm_ros2/xarm_moveit_config/config/scripts/add_posts.py
@@ -14,14 +14,26 @@ class SceneObjectAdder(Node):
 
         self.declare_parameter('frame_id', 'world')
 
-        # Shared box dimensions: 3 cm x 3 cm x 1 m
-        self.declare_parameter('box_size', [0.03, 0.03, 1.0])
+        # Original 4 upright posts: 3 cm x 3 cm x 1 m
+        self.declare_parameter('post_size', [0.03, 0.03, 1.0])
 
-        # Four box positions
-        self.declare_parameter('box_1_position', [-0.19, -0.292, 0.5])
-        self.declare_parameter('box_2_position', [-0.19,  0.875, 0.5])
-        self.declare_parameter('box_3_position', [ 0.61, -0.292, 0.5])
-        self.declare_parameter('box_4_position', [ 0.61,  0.875, 0.5])
+        self.declare_parameter('post_1_position', [-0.19, -0.292, 0.5])
+        self.declare_parameter('post_2_position', [-0.19,  0.875, 0.5])
+        self.declare_parameter('post_3_position', [ 0.61, -0.292, 0.5])
+        self.declare_parameter('post_4_position', [ 0.61,  0.875, 0.5])
+
+        # New horizontal members
+        self.declare_parameter('x_box_size', [0.75, 0.03, 0.03])
+        self.declare_parameter('y_box_size', [0.03, 1.13, 0.03])
+
+        # 3 long in X
+        self.declare_parameter('x_box_1_position', [0.21, -0.292, .985])
+        self.declare_parameter('x_box_2_position', [0.21,  0.2915, .985])
+        self.declare_parameter('x_box_3_position', [0.21,  0.875, .985])
+
+        # 2 long in Y
+        self.declare_parameter('y_box_1_position', [-0.19, 0.2915, .985])
+        self.declare_parameter('y_box_2_position', [ 0.61, 0.2915, .985])
 
         # Medium gray, alpha 0.8
         self.declare_parameter('color_rgba', [0.5, 0.5, 0.5, 0.8])
@@ -66,22 +78,49 @@ class SceneObjectAdder(Node):
             return
 
         frame_id = self.get_parameter('frame_id').value
-        box_size = self.get_parameter('box_size').value
+        post_size = self.get_parameter('post_size').value
+        x_box_size = self.get_parameter('x_box_size').value
+        y_box_size = self.get_parameter('y_box_size').value
         color_rgba = self.get_parameter('color_rgba').value
 
-        positions = [
-            self.get_parameter('box_1_position').value,
-            self.get_parameter('box_2_position').value,
-            self.get_parameter('box_3_position').value,
-            self.get_parameter('box_4_position').value,
+        post_positions = [
+            self.get_parameter('post_1_position').value,
+            self.get_parameter('post_2_position').value,
+            self.get_parameter('post_3_position').value,
+            self.get_parameter('post_4_position').value,
+        ]
+
+        x_positions = [
+            self.get_parameter('x_box_1_position').value,
+            self.get_parameter('x_box_2_position').value,
+            self.get_parameter('x_box_3_position').value,
+        ]
+
+        y_positions = [
+            self.get_parameter('y_box_1_position').value,
+            self.get_parameter('y_box_2_position').value,
         ]
 
         objects = []
         colors = []
 
-        for i, position in enumerate(positions, start=1):
-            obj_id = f'box_{i}'
-            obj = self.make_box(frame_id, obj_id, box_size, position)
+        for i, position in enumerate(post_positions, start=1):
+            obj_id = f'post_{i}'
+            obj = self.make_box(frame_id, obj_id, post_size, position)
+            color = self.make_color(obj_id, color_rgba)
+            objects.append(obj)
+            colors.append(color)
+
+        for i, position in enumerate(x_positions, start=1):
+            obj_id = f'x_box_{i}'
+            obj = self.make_box(frame_id, obj_id, x_box_size, position)
+            color = self.make_color(obj_id, color_rgba)
+            objects.append(obj)
+            colors.append(color)
+
+        for i, position in enumerate(y_positions, start=1):
+            obj_id = f'y_box_{i}'
+            obj = self.make_box(frame_id, obj_id, y_box_size, position)
             color = self.make_color(obj_id, color_rgba)
             objects.append(obj)
             colors.append(color)
@@ -94,7 +133,8 @@ class SceneObjectAdder(Node):
         self.pub.publish(scene)
 
         self.get_logger().info(
-            f'Published 4 boxes in frame "{frame_id}" with size={box_size} and rgba={color_rgba}'
+            f'Published 9 objects in frame "{frame_id}" '
+            f'(4 posts, 3 x-boxes, 2 y-boxes) with rgba={color_rgba}'
         )
 
         self.done = True
